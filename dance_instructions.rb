b0fa58a22a64e082828dc117261cd4dbdb06dc6f
m8itsjill <jillyusi@icloud.com>
2018-07-09 23:33:09 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,27 +1,22 @@
 
 # Code your methods below
 
-def Proper_Stance
+def starting_stance
   puts "Plants legs far apart, bend knees slightly and keep posture loose"
 end
 
 def Base_Footwork
   puts "Lift right foot"
   puts "Return right foot to the ground"
-  puts "Finishing with a small skip-step backward"
+  skip_step("left foot")
 end
 
-def Skip_Step_Left
-  puts "Lower left foot to the ground"
-  puts "Bounce left foot back up slightly, kicking it a few inches back"
-  puts "Left, Right, Left and Left"
-end
-
-def Skip_Step_Right
-  puts "Lower right foot to the ground"
-  puts "Bounce right foot back up slightly, kicking it a few inches back"
-  puts "Right, Left, Right and Right"
+def skip_step(starting_foot)
+  puts "Lower #{starting_foot} to the ground"
+  puts "Bounce #{starting_foot} back up slightly, kicking it a few inches back"
+  puts "Briefly raise the opposite foot, then the #{starting_foot} twice more!"
 end
 
 def Bob The Reins 
-  
+  puts "Holding your arms out in front of you,straight and at chest level"
+  puts "Cross your right wrist over your left and hold them together"
